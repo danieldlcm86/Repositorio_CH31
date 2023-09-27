c628d65e65f9235d38277afea98c839d6b204432
Daniel Maldonado <daniel_dlcmaldonado@hotmail.com>
2023-09-27 12:18:08 -0600

    update Exercise.java

--- JAVA04_LogicOperators/Exercise.java
+++ JAVA04_LogicOperators/Exercise.java
@@ -3,8 +3,8 @@ package logicOperators;
 public class Exercise {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		//Enrique, Paola y Mariana compran un iphone 15. Si el total de la compra supera los 25mil pesos, por lo tanto obitnenen un 10% de descuento. La tienda en estos momentos cuenta con una promoción de 12 meses sin intereses, por lo tanto, deciden pagar con la TDC de Mariana. ¿ Cuál sería el costo total de los equipos?¿Aplica descuento? ¿Cuál sería el costo total con descuento? ¿Cuánto pagarían mensualmente? 
 	}
 
 }
