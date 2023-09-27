907419fe4c2696c0a8683282fd2f10fbf4c3a3cc
Daniel Maldonado <daniel_dlcmaldonado@hotmail.com>
2023-09-27 12:28:18 -0600

    Update exercise

--- JAVA04_LogicOperators/Exercise.java
+++ JAVA04_LogicOperators/Exercise.java
@@ -4,7 +4,7 @@ public class Exercise {
 
 	public static void main(String[] args) {
 
-		//Enrique, Paola y Mariana compran un iphone 15. Si el total de la compra supera los 25mil pesos, por lo tanto obitnenen un 10% de descuento. La tienda en estos momentos cuenta con una promoción de 12 meses sin intereses, por lo tanto, deciden pagar con la TDC de Mariana. ¿ Cuál sería el costo total de los equipos?¿Aplica descuento? ¿Cuál sería el costo total con descuento? ¿Cuánto pagarían mensualmente? 
+		//Enrique, Paola y Mariana se dirigen a la MacStore y deciden comprar un iphone 15 cada uno. La tienda tiene una promoción por el mes patrio: "Si el total de tu compra supera los 25 mil pesos, obten un 10% de descuento". Enrique, Paola y Mariana deciden comprar los tres equipos con una sola tarjeta. Al llegar a caja, la cajera les menciona que la tienda en estos momentos cuenta con una promoción de 12 meses sin intereses, por lo tanto, deciden pagar con la TDC de Mariana. ¿Cuál sería el costo total por los 3 equipos sin descuento? ¿Aplicaría el descuento en su compra? ¿Cuál sería el costo total con el descuento aplicado? ¿Cuánto pagarían mensualmente?
 	}
 
 }
